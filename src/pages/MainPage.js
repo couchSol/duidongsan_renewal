de6e6e64e9e12f54397353cc2e7233f1f3bf3c5b
couchSol <sollee922@gmail.com>
2022-09-14 18:46:18 +0900

    add link to the main page

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function MainPage() {
   return (
     <main>
-        <div class="main-sm sm-only">
-            <div className="sm-about">
-                <div>공간</div>
-            </div>
-            <div className="sm-menu">
-                <div>메뉴</div>
-            </div>
-            <div className="sm-shop">
-                <div>상점</div>
-            </div>
-        </div> 
-        <div class="main-lg lg-only">          
-        </div>       
+      <div class="main-sm sm-only">
+        <Link to="/duidongsan_renewal/space">
+          <div className="sm-about">
+            <div>공간</div>
+          </div>
+        </Link>
+        <Link to="/duidongsan_renewal/menu">
+          <div className="sm-menu">
+            <div>메뉴</div>
+          </div>
+        </Link>
+        <Link to="/duidongsan_renewal/store">
+          <div className="sm-shop">
+            <div>상점</div>
+          </div>
+        </Link>
+      </div>
+      <div class="main-lg lg-only"></div>
     </main>
   );
 }
