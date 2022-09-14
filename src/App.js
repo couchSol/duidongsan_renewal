6a661b944b4cbf30cbbcabc0ce6b2bc047bc2e13
couchSol <sollee922@gmail.com>
2022-09-14 18:30:55 +0900

    fix route path

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import {Routes, Route} from "react-router-dom";
-import Header from './Header'
-import Footer from './Footer'
-import MainPage from './pages/MainPage'
-import SpacePage from './pages/SpacePage'
-import MenuPage from './pages/MenuPage'
-import StorePage from './pages/StorePage'
+import { Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import MainPage from "./pages/MainPage";
+import SpacePage from "./pages/SpacePage";
+import MenuPage from "./pages/MenuPage";
+import StorePage from "./pages/StorePage";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main>
-      <Routes>
-        <Route path="/" element={<MainPage />}></Route>
-        <Route path="/space" element={<SpacePage />}></Route>
-        <Route path="/menu" element={<MenuPage />}></Route>
-        <Route path="/store" element={<StorePage />}></Route>
-      </Routes>
+        <Routes>
+          <Route path="/duidongsan_renewal/" element={<MainPage />}></Route>
+          <Route
+            path="/duidongsan_renewal//space"
+            element={<SpacePage />}
+          ></Route>
+          <Route
+            path="/duidongsan_renewal//menu"
+            element={<MenuPage />}
+          ></Route>
+          <Route
+            path="/duidongsan_renewal//store"
+            element={<StorePage />}
+          ></Route>
+        </Routes>
       </main>
       <Footer />
     </div>
--- src/Header.js
+++ src/Header.js
@@ -1,76 +1,70 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 import logo from "./static/images/logo.png";
 
 function GnbDrawer() {
-    return (
-        <div className="gnb-drawer sm-only">
-            <a
-                className="gnb-icon-btn"
-                href="/"
-                aria-label="메뉴 서랍 열기"
-            >
-            <i className="ic-menu"></i>
-            </a>
-        </div>
-    )
+  return (
+    <div className="gnb-drawer sm-only">
+      <a className="gnb-icon-btn" href="/" aria-label="메뉴 서랍 열기">
+        <i className="ic-menu"></i>
+      </a>
+    </div>
+  );
 }
 
-function Gnb(){
-    return (
-        <nav className="lg-only">
-            <h2 className="visually-hidden">메뉴</h2>
-                <ul className="gnb-nav-list">
-                    <Link to='/space'><li className="gnb-nav-item">공간</li></Link>
-                    <Link to='/menu'><li className="gnb-nav-item">메뉴</li></Link>
-                    <Link to='/store'><li className="gnb-nav-item">상점</li></Link>
-                </ul>
-        </nav>
-    )
+function Gnb() {
+  return (
+    <nav className="lg-only">
+      <h2 className="visually-hidden">메뉴</h2>
+      <ul className="gnb-nav-list">
+        <Link to="/space">
+          <li className="gnb-nav-item">공간</li>
+        </Link>
+        <Link to="/menu">
+          <li className="gnb-nav-item">메뉴</li>
+        </Link>
+        <Link to="/store">
+          <li className="gnb-nav-item">상점</li>
+        </Link>
+      </ul>
+    </nav>
+  );
 }
 
-function GnbButton(){
-    return (
-        <div className="gnb-btn-group">
-            <a
-                className="gnb-icon-btn"
-                href="/"
-                aria-label="로그인 페이지로 이동"
-            >
-            <i className="ic-login"></i>
-            </a>
-            <a
-                className="gnb-icon-btn"
-                href="/"
-                aria-label="장바구니 페이지로 이동"
-            >
-            <i className="ic-cart"></i>
-            </a>
-        </div>
-    )
+function GnbButton() {
+  return (
+    <div className="gnb-btn-group">
+      <a className="gnb-icon-btn" href="/" aria-label="로그인 페이지로 이동">
+        <i className="ic-login"></i>
+      </a>
+      <a className="gnb-icon-btn" href="/" aria-label="장바구니 페이지로 이동">
+        <i className="ic-cart"></i>
+      </a>
+    </div>
+  );
 }
 
 function Header() {
   return (
     <header>
-        <div className="gnb">
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-4 col-lg-12">
-                        <div className="gnb-wrapper">
-                            <h1 className="logo">
-                                <Link to="/">
-                                    <img src={logo} alt="공간 뒷동산"/>
-                                </Link>
-                            </h1>
-                            <GnbDrawer />
-                            <Gnb/>
-                            <GnbButton/>
-                        </div>
-                    </div>
-                </div>
+      <div className="gnb">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-4 col-lg-12">
+              <div className="gnb-wrapper">
+                <h1 className="logo">
+                  <Link to="/duidongsan_renewal/">
+                    <img src={logo} alt="공간 뒷동산" />
+                  </Link>
+                </h1>
+                <GnbDrawer />
+                <Gnb />
+                <GnbButton />
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </header>
   );
 }
